5d6d3485b74fc80faf8399e5397bda8e5bd99c39
yunisaliosman <yunis.osman@student.moringaschool.com>
2022-12-05 06:33:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+
+const input = document.getElementById('input');
+
+function addingEventListener(){
+    function clickAlert(){
+        alert('I was touched')
+    }
+    input.addEventListener('click',clickAlert )
 }
+addingEventListener()
